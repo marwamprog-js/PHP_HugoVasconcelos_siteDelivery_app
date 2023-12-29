f9104116d509a12d890a0367924aebb5cc47b0ba
Marwam Malta <marwamprog.js@gmail.com>
2023-12-28 23:52:28 -0300

    feat: Ajustando os sliders

--- site/index.php
+++ site/index.php
@@ -3,22 +3,28 @@
 <!-- Swiper-->
 <section class="section swiper-container swiper-slider swiper-slider-modern" data-loop="true" data-autoplay="5000" data-simulate-touch="true" data-nav="true" data-slide-effect="fade">
     <div class="swiper-wrapper text-left">
-        <div class="swiper-slide context-dark" data-slide-bg="images/slider-1-1920x729.jpg">
+        
+        <div class="swiper-slide context-dark" data-slide-bg="images/banner/01.jpg">
             <div class="swiper-slide-caption">
                 <div class="container">
                     <div class="row justify-content-center justify-content-xxl-start">
                         <div class="col-md-10 col-xxl-6">
                             <div class="slider-modern-box">
-                                <h1 class="slider-modern-title"><span data-caption-animate="slideInDown" data-caption-delay="0">Organic Food</span></h1>
-                                <p data-caption-animate="fadeInRight" data-caption-delay="400">Herber provides local citizens and guests of our town with quality organic fruits, vegetables, and other products.</p>
-                                <div class="oh button-wrap"><a class="button button-primary button-ujarak" href="about-us.html" data-caption-animate="slideInLeft" data-caption-delay="400">Read more</a></div>
+                                <h1 class="slider-modern-title">
+                                    <span data-caption-animate="slideInDown" data-caption-delay="0">Burguer Freitas</span>
+                                </h1>
+                                <p data-caption-animate="fadeInRight" data-caption-delay="400">Venha conhecer nossos Hamburgues artesanais, os melhores da região.</p>
+                                <div class="oh button-wrap">
+                                    <a class="button button-primary button-ujarak" href="about-us.html" data-caption-animate="slideInLeft" data-caption-delay="400">Veja Mais</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="swiper-slide context-dark" data-slide-bg="images/slider-2-1920x729.jpg">
+
+        <div class="swiper-slide context-dark" data-slide-bg="images/banner/02.jpg">
             <div class="swiper-slide-caption">
                 <div class="container">
                     <div class="row justify-content-center justify-content-xxl-start">
@@ -33,7 +39,8 @@
                 </div>
             </div>
         </div>
-        <div class="swiper-slide" data-slide-bg="images/slider-3-1920x729.jpg">
+
+        <div class="swiper-slide" data-slide-bg="images/banner/01.jpg">
             <div class="swiper-slide-caption">
                 <div class="container">
                     <div class="row justify-content-center justify-content-xxl-start">
@@ -48,6 +55,7 @@
                 </div>
             </div>
         </div>
+
     </div>
     <!-- Swiper Navigation-->
     <div class="swiper-button-prev"></div>
